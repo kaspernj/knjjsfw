e89eabc18657befb9d07d1b270752d9bb59b806f
Kasper Johansen <k@spernj.org>
2011-07-07 22:10:11 +0200

    Fix a bug where a numeric input-text would be parsed more than 1 time.

--- locales.js
+++ locales.js
@@ -17,46 +17,53 @@ function number_format(number, decimals, dec_point, thousands_sep){
 }
 
 function number_in(numstr){
-	if(locale_strings["number_tsep"].length){
-		while(numstr.indexOf(locale_strings["number_tsep"], "") != -1){
-			numstr = numstr.replace(locale_strings["number_tsep"], "");
-		}
-	}
-	
-	numstr = numstr.replace(locale_strings["number_decimal"], ".");
-	numstr = parseFloat(numstr);
-	return numstr;
+  if (locale_strings["number_tsep"].length){
+    while(numstr.indexOf(locale_strings["number_tsep"], "") != -1){
+      numstr = numstr.replace(locale_strings["number_tsep"], "");
+    }
+  }
+  
+  numstr = numstr.replace(locale_strings["number_decimal"], ".");
+  numstr = parseFloat(numstr);
+  
+  return numstr;
 }
 
 function number_out(number, decimals){
-	return number_format(number, decimals, locale_strings["number_decimal"], locale_strings["number_tsep"]);
+  return number_format(number, decimals, locale_strings["number_decimal"], locale_strings["number_tsep"]);
 }
 
 function locales_setup(args){
-	var head = document.getElementsByTagName("head")[0] || document.body;
-	var script = document.createElement("script");
-	script.type = "text/javascript";
-	
-	if (args["url"]){
-		script.src = args["url"] + "/jquery.sprintf-1.0.3.js";
-	}else{
-		script.src = "https://www.kaspernj.org/js/jquery.sprintf-1.0.3.js";
-	}
-	
-	head.appendChild(script);
+  var head = document.getElementsByTagName("head")[0] || document.body;
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  
+  if (args["url"]){
+    script.src = args["url"] + "/jquery.sprintf-1.0.3.js";
+  }else{
+    script.src = "https://www.kaspernj.org/js/jquery.sprintf-1.0.3.js";
+  }
+  
+  head.appendChild(script);
 }
 
 $(document).ready(function(){
-	$(".input_numeric").each(function(no, rawele){
-		ele = $(rawele);
-		formele = $(ele.closest("form"));
-		formele.submit(function(){
-			$(".input_numeric", formele).each(function(){
-				thisele = $(this);
-				val = thisele.val();
-				newval = number_in(val);
-				thisele.val(newval);
-			});
-		});
-	});
+  $(".input_numeric").each(function(no, rawele){
+    ele = $(rawele);
+    formele = $(ele.closest("form"));
+    formele_raw = formele.get(0);
+    
+    if (!formele.attr("knjlocales_treated")){
+      formele.attr("knjlocales_treated", "true");
+      
+      formele.submit(function(){
+        $(".input_numeric", formele).each(function(){
+          thisele = $(this);
+          val = thisele.val();
+          newval = number_in(val);
+          thisele.val(newval);
+        });
+      });
+    }
+  });
 });
